551b943725fc0d6e4395036b5c4531e25a72a8a3
Sapna Sunil Rokade <srokade@altimetrik.com>
2024-11-21 10:48:31 +0530

    Updated Project

--- src/test/java/com/crm/qa/testcases/runner/RunnerFile.java
+++ src/test/java/com/crm/qa/testcases/runner/RunnerFile.java
@@ -4,9 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resource/feature/sample.feature",  // Path to feature files
-    glue = "com.crm.qa.testcases.loginPage.StepDefination",  // Path to step definition classes
-    plugin = {"pretty", "html:target/cucumber-reports"}  // Optional report generation
+		publish = true,
+    features = "src/test/resource/feature/sample.feature",
+    plugin = {"pretty", "html:target/cucumber-reports"},// Path to feature files
+    glue = "com/crm/qa/testcases/loginPage",  // Path to step definition classes
+   tags = "@test"
 )
 public class RunnerFile extends AbstractTestNGCucumberTests {
     // Empty class as it uses annotations to run tests
